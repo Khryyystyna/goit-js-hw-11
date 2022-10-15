6659c7d88cd0d68f5ca78c1eba1ac6911300cd0e
Khrystyna Halaiko <gayka1818@gmail.com>
2022-10-15 11:50:07 +0300

    Add

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -1,33 +1,16 @@
-// import axios from 'axios';
-
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// // axios.defaults.headers.common['Authorization'] =
-// //   'API key 30567528-24ce7a845f498c2eba342043e';
 
 export class PixabayAPI {
   #page = 1;
   #totalHits = 0;
   #perPage = 20;
   #query = '';
-  // #params = {
-  //   params: {
-  //     imageType: "photo",
-  //     orientation: "horizontal",
-  //     safeSearch: "true",
-  //     key:'30567528-24ce7a845f498c2eba342043e',
-  //   }
-  
+
 
   async getPhotos() {
     const key = '30567528-24ce7a845f498c2eba342043e';
     const imageType = "photo";
     const orientation = "horizontal";
     const safeSearch = "true";
-
-    // const urlAxios = `?q=${this.#query}&pretty=true&page=${this.#page}&tottal_hits=${this.#totalHits}&per_page=${this.#perPage}`;
-
-    // const { data } = await axios.get(urlAxios, this.#params);
-    // return data;
 
     const url = `https://pixabay.com/api/?key=${key}&q=${this.#query}&image_type=${imageType}&pretty=true&orientation=${orientation}&safe_search=${safeSearch}&page=${this.#page}&tottal_hits=${this.#totalHits}&per_page=${this.#perPage}`;
 
